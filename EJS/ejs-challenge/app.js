45a581ca92be5dcca22b8ae7f8eb7857b8e2c4dd
chavda-mitul <20dce020@charusat.edu.in>
2022-06-26 17:56:55 +0530

    added the read more in the paragraph which redirect to its actual page

--- EJS/ejs-challenge/app.js
+++ EJS/ejs-challenge/app.js
@@ -9,17 +9,30 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const lowerCase = require('lodash.lowercase');
 app.use(bodyParser.urlencoded({extended : true}));
 
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static("public"));  
 const post = [];
 
 app.get('/',(req,res)=>{
   res.render('home',{homeStartingContent:homeStartingContent , post:post});
+});
+
+app.get('/post/:postId',(req,res)=>{
+  var tittle = lowerCase(req.params.postId);
+  let postTittle='',postContent='';
+  post.forEach((p)=>{
+    if(tittle  == lowerCase(p.tittle)){
+      postTittle = p.tittle;
+      postContent = p.content;
+    }
+  });
+  res.render('post',{Tittle : postTittle , Content : postContent});
 });
 
 app.get('/about',(req,res)=>{
